1da597a8a7ff334e62d6f04a3a3d10f2bd5bf677
yang.zhou <yang.zhou@futuremove.cn>
2019-11-12 11:38:25 +0800

    spring

--- src/main/java/com/springtest/zy/spring/UserDemo.java
+++ src/main/java/com/springtest/zy/spring/UserDemo.java
@@ -1,12 +1,16 @@
 package com.springtest.zy.spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserDemo {
 
     @Test
     public void testSpring() {
-
+        ApplicationContext applicationContext=new ClassPathXmlApplicationContext("SpringApplication.xml");
+        UserImpement user = (UserImpement) applicationContext.getBean("user");
+        user.haha();
     }
 
     ;
