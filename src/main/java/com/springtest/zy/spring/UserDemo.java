84923e28163e1eb5a59cb01972bcded7cca7ef0b
zy <814575934@qq.com>
2019-11-27 21:24:36 +0800

    spring

--- src/main/java/com/springtest/zy/spring/UserDemo.java
+++ src/main/java/com/springtest/zy/spring/UserDemo.java
@@ -3,6 +3,10 @@ package com.springtest.zy.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 public class UserDemo {
 
@@ -15,6 +19,10 @@ public class UserDemo {
         UserImpement1 user1 = (UserImpement1) applicationContext.getBean("user1");
         user1.haha();
     }
-
-    ;
+//    @RequestMapping(value = "/getFactoryData")
+//    public @ResponseBody
+//    List getFactoryData(){
+//        //调用service，传入companyId条件，返回的是一个list
+//        return statService.getFactoryData(companyId);
+//    }
 }
