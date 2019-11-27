f748095cec42b6bc9108320e7ffff82199b0cd04
yang.zhou <yang.zhou@futuremove.cn>
2019-11-27 09:12:16 +0800

    spring

--- src/main/webapp/jiebao/components/loadingAnimation/loadingAnimation.js
+++
@@ -1,9 +0,0 @@
-var animation = document.getElementById("loadingAnimationContainer");
-
-function toggleAnimation() {
-	if (animation.style.display === "block"){
-		animation.style.display = "none"; 
-	} else {
-		animation.style.display = "block";
-	}
-};
--- src/main/webapp/jiebao/components/scrollbox/scrollbox.js
+++
@@ -1,9 +0,0 @@
-var els=document.querySelectorAll('.scrollToTop');
-Array.prototype.forEach.call(els, function(el, i){
-	el.addEventListener('click', function(){
-		target=el.dataset.target;
-		document.getElementById(target).scrollIntoView({
-		  behavior: 'smooth'
-		});
-	});
-});
