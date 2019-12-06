38146af2a0171e3add004592a75d5ba1198d77c6
zy <814575934@qq.com>
2019-12-06 23:15:01 +0800

    spring

--- src/main/java/com/springtest/zy/spring/jdbc/JdbcDemo.java
+++ src/main/java/com/springtest/zy/spring/jdbc/JdbcDemo.java
@@ -4,11 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 @ContextConfiguration("classpath:SpringApplication.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -34,7 +39,43 @@ public class JdbcDemo {
     public void demo2() {
         //创建jdbc
 
-        jdbcTemplate.update("insert into account values (null,?,?)", "new", 111);
+//        jdbcTemplate.update("insert into account values (null,?,?)", "new", 111);
+        //xiugai
+//        jdbcTemplate.update("update account set name =? ,money=? where id =?","sss",2222,3);
+
+        //删除
+//        jdbcTemplate.update("delete from  account where id=?",2);
+
+        //查询
+//        String s = jdbcTemplate.queryForObject("select name from account where id =?", String.class, 3);
+//        System.out.println(s+"");
+
+//        Long aLong = jdbcTemplate.queryForObject("select count(*) from account",Long.class);
+//        System.out.println(aLong+"");
+
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from account");
+        System.out.println(maps.toArray().toString());
+    }
+
+    @Test
+    // 查询多条记录
+    public void demo7(){
+        List<User> list = jdbcTemplate.query("select * from account", new MyRowMapper());
+        for (User account : list) {
+            System.out.println(account);
+        }
+    }
+
+    class MyRowMapper implements RowMapper<User> {
+
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User account = new User();
+            account.setId(rs.getInt("id"));
+            account.setName(rs.getString("name"));
+            account.setMoney(rs.getDouble("money"));
+            return account;
+        }
 
     }
 }
---
+++ src/main/java/com/springtest/zy/spring/jdbc/User.java
@@ -0,0 +1,42 @@
+package com.springtest.zy.spring.jdbc;
+
+public class User {
+
+    String name;
+    double money;
+    int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", money=" + money +
+                ", id=" + id +
+                '}';
+    }
+}
