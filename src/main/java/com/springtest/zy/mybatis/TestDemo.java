a3624c4f4dd75195857e02dbb02f0fbc5f5db4d0
zy <814575934@qq.com>
2019-12-09 22:40:25 +0800

    spring的事务

---
+++ src/main/java/com/springtest/zy/mybatis/TestDemo.java
@@ -0,0 +1,26 @@
+package com.springtest.zy.mybatis;
+
+        import com.springtest.zy.spring.jdbc.User;
+        import org.apache.ibatis.io.Resources;
+        import org.apache.ibatis.session.SqlSession;
+        import org.apache.ibatis.session.SqlSessionFactory;
+        import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+        import org.junit.Test;
+
+        import java.io.IOException;
+        import java.io.InputStream;
+
+public class TestDemo {
+    @Test
+    public void test() throws IOException {
+            String resou="mapper/sqlMapConfig.xml";
+        InputStream resourceAsStream = Resources.getResourceAsStream(resou);
+        //创建session工厂
+
+        SqlSessionFactory build = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+        SqlSession sqlSession = build.openSession();
+        User selectUser = sqlSession.selectOne("selectUser", 2);
+        System.out.println(selectUser.toString());
+    }
+}
